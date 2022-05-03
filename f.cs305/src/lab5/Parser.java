a4ab0af311fe4fff2c6bc6fe59126fd47870a08e
Лхагважаргал <B190910051@must.edu.mn>
2022-05-03 23:01:14 +0800

    .

--- f.cs305/src/lab5/Parser.java
+++ f.cs305/src/lab5/Parser.java
@@ -304,7 +304,7 @@ public class Parser {
     
     public static void main(String args[]) {
     	System.out.println("Begin parsing... \n");
-        Parser parser  = new Parser(new Lexer("C:\\Users\\gdwoo\\eclipse-workspace\\f.cs305\\src\\lab5\\newton.cpp"));
+        Parser parser  = new Parser(new Lexer("C:\\Users\\gdwoo\\git\\f.cs305_helniizarchim\\f.cs305\\src\\lab5\\newton.cpp"));
         Program prog = parser.program();        
         prog.display();           // display abstract syntax tree
     } //main
